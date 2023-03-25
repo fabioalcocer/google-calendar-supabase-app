2e0bbe6cf2fbe491e7d8cfc672b185c897ca66c9
fabio alcocer <fabioalcocersejas17@gmail.com>
2023-03-25 01:27:50 -0400

    add create event method, tailwind and buttons

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,19 @@
-import './App.css'
 import {
   useSession,
   useSupabaseClient,
   useSessionContext,
 } from '@supabase/auth-helpers-react'
+import { useState } from 'react'
+import DateTimePicker from 'react-datetime-picker'
+import GoogleBtn from './components/GoogleBtn'
+import CreateEventBtn from './components/CreateEventBtn'
 
 function App() {
+  const [start, setStart] = useState(new Date())
+  const [end, setEnd] = useState(new Date())
+  const [eventName, setEventName] = useState('')
+  const [eventDescription, setEventDescription] = useState('')
+
   const session = useSession()
   const supabase = useSupabaseClient()
   const { isLoading } = useSessionContext()
@@ -32,11 +40,70 @@ function App() {
     await supabase.auth.signOut()
   }
 
+  const createCalendarEvent = async () => {
+    const event = {
+      summary: eventName,
+      description: eventDescription,
+      start: {
+        dateTime: start.toISOString(),
+        timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+      },
+      end: {
+        dateTime: end.toISOString(),
+        timeZone: Intl.DateTimeFormat().resolvedOptions().timeZone,
+      },
+    }
+
+    const res = await fetch(
+      'https://www.googleapis.com/calendar/v3/calendars/primary/events',
+      {
+        method: 'POST',
+        headers: {
+          Authorization: `Bearer ${session?.provider_token}`,
+        },
+        body: JSON.stringify(event),
+      }
+    )
+      .then((data) => {
+        return data.json()
+      })
+      .then((data) => console.log(data))
+
+    return res
+  }
+
   console.log(session)
 
   return (
-    <div className='App'>
-      <h1>Google Calendar + Supabase</h1>
+    <div className='App min-h-screen bg-zinc-900 grid place-content-center gap-5 text-zinc-50'>
+      <h1 className='font-extrabold'>Google Calendar + Supabase</h1>
+      <div>
+        <p>Start of your event</p>
+        <DateTimePicker onChange={setStart} value={start} />
+        <label>Event name</label>
+        <input
+          value={eventName}
+          type='text'
+          onChange={(e) => setEventName(e.target.value)}
+          className='text-zinc-900'
+        />
+      </div>
+      <div>
+        <p>End of your event</p>
+        <DateTimePicker onChange={setEnd} value={end} />
+        <label>Event description</label>
+        <input
+          value={eventDescription}
+          type='text'
+          onChange={(e) => setEventDescription(e.target.value)}
+          className='text-zinc-900'
+        />
+      </div>
+
+      <button onClick={() => createCalendarEvent()}>
+        Create Calendar Event
+      </button>
+
       <div>
         {session ? (
           <>
@@ -48,6 +115,8 @@ function App() {
             Sign In with Google
           </button>
         )}
+        <GoogleBtn />
+        <CreateEventBtn />
       </div>
     </div>
   )
---
+++ src/components/CreateEventBtn.tsx
@@ -0,0 +1,13 @@
+function CreateEventBtn() {
+  return (
+    <button className="create">
+      <span className='txt'>Create Event</span>
+      <span className='txt2'>Created!</span>
+      <span className='loader-container'>
+        <span className='loader'></span>
+      </span>
+    </button>
+  )
+}
+
+export default CreateEventBtn
